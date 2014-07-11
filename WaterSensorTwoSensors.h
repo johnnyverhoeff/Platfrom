3f5dabc71e0822ee8f3e86b8c15acb629a626c1c
Johnny Verhoeff <j.verhoeff@me.com>
2014-07-11 19:05:01 +0200

    added names of params also to header files

--- WaterSensorTwoSensors.h
+++ WaterSensorTwoSensors.h
@@ -8,7 +8,7 @@ protected:
 	int _upper_pin;
 
 public:
-	WaterSensorTwoSensors(int, int);
+	WaterSensorTwoSensors(int lower_pin, int upper_pin);
 	bool is_water_rising();
 	bool is_water_dropping();
 	bool is_water_steady();
