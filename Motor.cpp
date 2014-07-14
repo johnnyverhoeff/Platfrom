d49385b7df597c84c041664fd5a4053fa3430e23
Johnny Verhoeff <j.verhoeff@me.com>
2014-07-14 20:38:49 +0200

    added debug for motor.cpp

--- Motor.cpp
+++ Motor.cpp
@@ -2,6 +2,11 @@
 #include "Arduino.h"
 
 Motor::Motor(int down_pin, int up_pin, int SDI, int SCK, int CS) {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"Motor::Motor called with params: down_pin: ");
+		Serial.print(down_pin); Serial.print(", up_pin: "); Serial.println(up_pin);
+	#endif
+
 	_move_down_pin = down_pin;
 	_move_up_pin = up_pin;
 
@@ -16,6 +21,11 @@ Motor::Motor(int down_pin, int up_pin, int SDI, int SCK, int CS) {
 }
 
 void Motor::move(movement movement, int speed) {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"Motor::move called with params: movement: ");
+		Serial.print(movement); Serial.print(", speed: "); Serial.println(speed);
+	#endif
+
 	switch (movement) {
 		case move_up:
 			digitalWrite(_move_up_pin, HIGH);
@@ -34,29 +44,61 @@ void Motor::move(movement movement, int speed) {
 }
 
 void Motor::down() {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("\"Motor::down called");
+	#endif
+
 	down(full_speed);
 }
 
 void Motor::down(speed speed) {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"Motor::down called with params: speed: ");
+		Serial.print(speed);
+	#endif
+
 	down((int)speed);
 }
 
 void Motor::down(int speed) {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"Motor::down called with params: speed: ");
+		Serial.print(speed);
+	#endif
+
 	move(move_down, speed);
 }
 
 void Motor::up() {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("\"Motor::up called");
+	#endif
+
 	up(full_speed);
 }
 
 void Motor::up(speed speed) {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"Motor::up called with params: speed: ");
+		Serial.print(speed);
+	#endif
+
 	up((int)speed);
 }
 
 void Motor::up(int speed) {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"Motor::up called with params: speed: ");
+		Serial.print(speed);
+	#endif
+
 	move(move_up, speed);
 }
 
 void Motor::stop() {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("\"Motor::stop called");
+	#endif
+
 	move(stop_moving, zero_speed);
 }
