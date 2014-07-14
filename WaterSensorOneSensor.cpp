9ae937f2a2126dcd011bcf6732f97690b5371a25
Johnny Verhoeff <j.verhoeff@me.com>
2014-07-14 19:33:53 +0200

    added debug via serial in all methods from water sensors

--- WaterSensorOneSensor.cpp
+++ WaterSensorOneSensor.cpp
@@ -3,29 +3,58 @@
 
 WaterSensorOneSensor::WaterSensorOneSensor(int pin) : _pin(pin) {
 	pinMode(_pin, INPUT);
+
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorOneSensor\" Constructor called with pin: ");
+		Serial.println(_pin);
+	#endif
 }
 
 bool WaterSensorOneSensor::is_water_rising() {
-	return digitalRead(_pin);
+	bool result = digitalRead(_pin);
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorOneSensor::is_water_rising\" called with outcome: ");
+		Serial.println(result);
+	#endif
+	return result;
 }
 
 bool WaterSensorOneSensor::is_water_dropping() {
-	return !is_water_rising();
+	bool result = !is_water_rising();
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorOneSensor::is_water_dropping\" called with outcome: ");
+		Serial.println(result);
+	#endif
+	return result;
 }
 
 bool WaterSensorOneSensor::is_water_steady() {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorOneSensor::is_water_steady\" called with outcome: ");
+		Serial.println(false);
+	#endif
 	return false;
 }
 
 bool WaterSensorOneSensor::has_reached_position(Vlonder::moving_state moving_state) {
+	bool result = false;
 	switch (moving_state) {
 		case Vlonder::moving_up:
-			return is_water_dropping();
+			result = is_water_dropping();
 			break;
 		case Vlonder::moving_down:
-			return is_water_rising();
+			result = is_water_rising();
 			break;
 		default:
-			return false;
+			result = false;
 	}
+
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorOneSensor::has_reached_position\" called with param: ");
+		Serial.print(moving_state);
+		Serial.print(", with outcome: ");
+		Serial.println(result);
+	#endif
+
+	return result;
 }
--- WaterSensorTwoSensors.cpp
+++ WaterSensorTwoSensors.cpp
@@ -5,20 +5,47 @@ WaterSensorTwoSensors::WaterSensorTwoSensors(int lower_pin, int upper_pin) :
 	WaterSensorOneSensor(lower_pin), _upper_pin(upper_pin) {
 	pinMode(_pin, INPUT);
 	pinMode(_upper_pin, INPUT);
+
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorTwoSensor\" Constructor called with pins: ");
+		Serial.print(_pin); Serial.print(" and "); Serial.println(_upper_pin);
+	#endif
 }
 
 bool WaterSensorTwoSensors::is_water_rising() {
-	return digitalRead(_pin) && digitalRead(_upper_pin);
+	bool result = digitalRead(_pin) && digitalRead(_upper_pin);
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorTwoSensor::is_water_rising\" called with outcome: ");
+		Serial.println(result);
+	#endif
+	return result;
 }
 
 bool WaterSensorTwoSensors::is_water_dropping() {
-	return !digitalRead(_pin) && !digitalRead(_upper_pin);
+	bool result = !digitalRead(_pin) && !digitalRead(_upper_pin);
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorTwoSensor::is_water_dropping\" called with outcome: ");
+		Serial.println(result);
+	#endif
+	return result;
 }
 
 bool WaterSensorTwoSensors::is_water_steady() {
-	return digitalRead(_pin) && !digitalRead(_upper_pin);
+	bool result = digitalRead(_pin) && !digitalRead(_upper_pin);
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorTwoSensor::is_water_steady\" called with outcome: ");
+		Serial.println(result);
+	#endif
+	return result;
 }
 
 bool WaterSensorTwoSensors::has_reached_position(Vlonder::moving_state moving_state) {
-	return is_water_steady();
+	bool result = is_water_steady();
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.print("\"WaterSensorTwoSensor::has_reached_position\" called with param: ");
+		Serial.print(moving_state);
+		Serial.print(", with outcome: ");
+		Serial.println(result);
+	#endif
+	return result;
 }
