595b1ada11b22370f5c279b934fd12777c9dfcb2
Johnny Verhoeff <j.verhoeff@me.com>
2014-07-18 17:39:22 +0200

    added some simple debug statements

--- WaterMeasurer.cpp
+++ WaterMeasurer.cpp
@@ -2,6 +2,10 @@
 #include "Arduino.h"
 
 WaterMeasurer::WaterMeasurer() {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("WaterMeasurer::WaterMeasurer called");
+	#endif
+
 	_reset();
 
 	_sample_period = 60;
@@ -13,11 +17,17 @@ WaterMeasurer::WaterMeasurer() {
 }
 
 void WaterMeasurer::_reset_hits() {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("WaterMeasurer::_reset_hits called");
+	#endif
 	_water_dropping_hits = 0;
 	_water_rising_hits = 0;
 }
 
 void WaterMeasurer::_reset() {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("WaterMeasurer::_reset called");
+	#endif
 	_reset_hits();
 	_sample_in_progress = false;
 	_sample_counter = 0;
@@ -25,6 +35,10 @@ void WaterMeasurer::_reset() {
 }
 
 bool WaterMeasurer::measure_sample(unsigned long start_time) {
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("WaterMeasurer::measure_sample called");
+	#endif
+
 	if (millis() - start_time < _sample_time * 100) {
 		_sample_in_progress = true;
 		return false;
@@ -43,7 +57,9 @@ bool WaterMeasurer::measure_sample(unsigned long start_time) {
 }
 
 WaterMeasurer::measure_results WaterMeasurer::get_measure_results() {
-
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("WaterMeasurer::get_measure_results called");
+	#endif
 	if (_sample_counter < _total_samples_needed) { // if not done collecting all samples
 
 		if (!_sample_in_progress) // if no sample is in progress, set the start time and call the measure method
