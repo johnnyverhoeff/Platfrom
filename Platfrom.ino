7348905af42a978058d6be116bcd4fa9ed359e6d
Johnny Verhoeff <j.verhoeff@me.com>
2014-08-02 16:44:18 +0200

    added new program states and button logic to invoke those states

--- Platfrom.ino
+++ Platfrom.ino
@@ -1,13 +1,14 @@
 #include "Vlonder.h"
 #include "WaterSensorTwoSensors.h"
 #include "SPI\SPI.h"
+#include "ClickButton.h"
 
 #pragma region Defines
 
 //#define DEBUG_VIA_SERIAL
 
 #define LIMIT_SWITCH_INTERRUPT_NUMBER 4 // dont know which interrupt number yet
-#define REMOTE_CONTROL_INTERRUPT_NUMBER 5 // dont know which interrupt number yet 
+#define REMOTE_CONTROL_INTERRUPT_NUMBER 3 // dont know which interrupt number yet 
 
 #define HIGH_BOAT_UPPER_PIN 10 // dont know
 #define HIGH_BOAT_LOWER_PIN 11 // dont know
@@ -34,12 +35,23 @@ WaterSensorTwoSensors high_boat_sensor(HIGH_BOAT_LOWER_PIN, HIGH_BOAT_UPPER_PIN,
 
 enum program_states {
 	none,
+
 	reach_active_water_sensor,
 	reach_and_control_vlonder_on_active_water_sensor,
 	control_vlonder_on_active_water_sensor,
+
+	reach_upper_limit_switch,
+	reach_lower_limit_switch,
+
+	remote_control_manual_up,
+	remote_control_manual_down,
+
+
 };
 
 program_states program_state;
+
+ClickButton buttons[] = { ClickButton(2, HIGH), ClickButton(3, HIGH) };
 
 void setup() {
 	SPI.begin();
@@ -48,11 +60,14 @@ void setup() {
 	Serial.begin(9600);
 
 	Serial.println("Serial has begun");
+	#ifdef DEBUG_VIA_SERIAL
+		Serial.println("DEBUG_VIA_SERIAL");
+	#endif
 
-	program_state = control_vlonder_on_active_water_sensor;
+	program_state = none;// reach_and_control_vlonder_on_active_water_sensor;
 	Vlonder::Begin();
 
-	setup_ISRs();
+	//setup_ISRs();
 
 	Vlonder::set_active_water_sensor(&high_boat_sensor);
 }
@@ -60,12 +75,12 @@ void setup() {
 void loop() {
 	#ifdef DEBUG_VIA_SERIAL
 		if (flag_interrupt) {
-			Serial.println(interrupt_msg);
+			Serial.println(F("an interrupt"));
 			flag_interrupt = false;
 		}
 	#endif
 
-	if (flag_remote_control_button_pressed)
+	//if (flag_remote_control_button_pressed)
 		handle_remote_control();
 	
 	switch (program_state) {
@@ -84,6 +99,24 @@ void loop() {
 			Vlonder::control_at_active_water_sensor();
 			break;
 
+		case reach_upper_limit_switch:
+			if (Vlonder::reach_upper_limit_switch())
+				program_state = none;
+			break;
+
+		case reach_lower_limit_switch:
+			if (Vlonder::reach_lower_limit_switch())
+				program_state = none;
+			break;
+
+		case remote_control_manual_up:
+			Vlonder::up();
+			break;
+
+		case remote_control_manual_down:
+			Vlonder::down();
+			break;
+
 		case none: 
 		default:
 			Vlonder::stop();
@@ -92,7 +125,29 @@ void loop() {
 }
 
 void handle_remote_control(void) {
-	
+	for (int i = 0; i < 2; i++)
+		buttons[i].Update();
+
+	if (buttons[0].clicks == -1)
+		program_state = remote_control_manual_up;
+
+	if (program_state == remote_control_manual_up & !buttons[0].depressed)
+		program_state = none;
+
+	if (buttons[1].clicks == -1)
+		program_state = remote_control_manual_down;
+
+	if (program_state == remote_control_manual_down & !buttons[1].depressed)
+		program_state = none;
+
+
+
+	if (buttons[0].clicks == -2)
+		program_state = reach_upper_limit_switch;
+
+	if (buttons[1].clicks == -2)
+		program_state = reach_lower_limit_switch;
+
 }
 
 
@@ -111,26 +166,29 @@ void setup_ISRs(void) {
 	attachInterrupt(LIMIT_SWITCH_INTERRUPT_NUMBER, ISR_limit_switch_reached, RISING);
 
 	// attach interrupt to all remote control buttons to occur when input changes
-	attachInterrupt(REMOTE_CONTROL_INTERRUPT_NUMBER, ISR_remote_control, CHANGE);
+	attachInterrupt(REMOTE_CONTROL_INTERRUPT_NUMBER, ISR_remote_control, RISING);
 }
 
 
 void ISR_limit_switch_reached(void) {
 	#ifdef DEBUG_VIA_SERIAL
 		flag_interrupt = true;
-		interrupt_msg = "ISR_limit_switch_reached";
+		//interrupt_msg = "ISR_limit_switch_reached";
 	#endif
 
-	// dont know yet to do this or global variable and in loop call stop....
 	Vlonder::stop();
+	program_state = none;
 }
 
 void ISR_remote_control(void) {
 	#ifdef DEBUG_VIA_SERIAL
 		flag_interrupt = true;
-		interrupt_msg = "ISR_remote_control";
+		//interrupt_msg = "ISR_remote_control";
 	#endif
 
+		//Vlonder::stop(); 
+		//program_state = none;
+
 	// inverses state of this flag.
-	flag_remote_control_button_pressed = !flag_remote_control_button_pressed;
+	//flag_remote_control_button_pressed = !flag_remote_control_button_pressed;
 }
