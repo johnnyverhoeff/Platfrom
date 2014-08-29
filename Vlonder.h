e0948bb420814a9d3661ccf6552672466524f067
johnnyverhoeff <j.verhoeff@me.com>
2014-08-29 14:28:03 +0200

    edited pins for correct leds and buttons

--- Vlonder.h
+++ Vlonder.h
@@ -31,11 +31,11 @@ namespace Vlonder {
 			Serial.println("Vlonder::Vlonder called");
 		#endif
 		// both limit switches on interrupt pins, dont know which ones yet
-		_lower_limit_switch = new LimitSwitch(30);
+		_lower_limit_switch = new LimitSwitch(32);
 		_upper_limit_switch = new LimitSwitch(31);
 
-		_left_motor = new Motor(40, 41, 42);
-		_right_motor = new Motor(43, 44, 45);
+		_left_motor = new Motor(38, 44, 47);
+		_right_motor = new Motor(30, 34, 49);
 		
 		_moving_state = vlonder_stopped;
 	}
